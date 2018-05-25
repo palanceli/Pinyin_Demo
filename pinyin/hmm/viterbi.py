88b6cb29617de08bddbca0cfaebebcde33132df5
palance <palanceli@163.com>
2018-05-25 20:45:22 +0800

    commit changes

--- pinyin/hmm/viterbi.py
+++ pinyin/hmm/viterbi.py
@@ -2,6 +2,7 @@
 """
     viterbi算法实现
 """
+import logging
 import sys
 sys.path.append('../..')
 from pinyin.model import Emission, Transition
@@ -15,15 +16,18 @@ def viterbi(pinyin_list):
         pinyin_list (list): 拼音列表
     """
     start_char = Emission.join_starting(pinyin_list[0])
+    logging.info(start_char)
     V = {char: prob for char, prob in start_char}
 
     for i in range(1, len(pinyin_list)):
         pinyin = pinyin_list[i]
+        logging.info(pinyin)
 
         prob_map = {}
         for phrase, prob in V.items():
             character = phrase[-1]
             result = Transition.join_emission(pinyin, character)
+            logging.info(result)
             if not result:
                 continue
 
@@ -38,6 +42,8 @@ def viterbi(pinyin_list):
 
 
 if __name__ == '__main__':
+    logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=logFmt, datefmt='%H:%M',)
     while 1:
         string = input('input:')
         pinyin_list = string.split()
